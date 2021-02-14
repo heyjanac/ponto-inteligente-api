37293491188de932159ff033addcbad52c4c6809
heyjanac <heyjanac@gmail.com>
2021-02-13 23:59:58 -0300

    inclusao repository, service e controller

---
+++ src/main/java/com/heyjanac/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,11 @@
+package com.heyjanac.pontointeligente.api.enums;
+
+/**  Tipo de lancamento de hora de um funcionario. */
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA;
+}
